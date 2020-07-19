8fc8ca623a64778741df086e4046e33b71b124c3
Jacob Evans <cloud887@gmail.com>
2020-07-19 16:47:51 -0500

    Server changed to GQL

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate juniper;
+
 use std::io;
+use std::sync::Arc;
 
-
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
 
 
 mod gql_schema;
@@ -28,6 +30,24 @@ fn main() -> io::Result<()> {
         .run()
 }
 
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,) -> impl Future<Item = HttpResponse , Error = Error>{
+        web::block(move || {
+            let res = data.execute(&st, &());
+            Ok::<_,serde_json::error::Error>(serde_json::to_string(&res)?)
+            .map_err(Error::from)
+            .and_then(|user| {
+                Ok(HttpResponse::OK()
+            .content_type("application/json")
+            .body(user))
+            })
+        })
+}
+
+fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://localhost:4321/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
 }
