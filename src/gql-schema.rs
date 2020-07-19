7065068a4f2e710185274df8811e4ada8f6cc66e
Jacob Evans <cloud887@gmail.com>
2020-07-19 15:30:06 -0500

    Schema file created

--- src/gql-schema.rs
+++ src/gql-schema.rs
@@ -1,3 +1,33 @@
 use juniper::{EmptyMutation, RootNode};
 
+struct Member {
+    id: i32,
+    name: String
+}
 
+#[juniper::object(description = "Member of a team")]
+impl Member {
+    pub fn id(&self) -> i32{
+        self.id
+    }
+    pub fn name(&self) -> String{
+        self.name.as_str()
+    }
+}
+
+pub struct QueryRoot;
+
+#[juniper::object]
+impl QueryRoot {
+    fn members() -> Vec<Member>{
+        vec![ 
+          Member {
+            id: 1,
+            name: "Link".to_owned(),
+          },
+          Member {
+            id: 2,
+            name: "Mario".to_owned(),
+          }]
+    }
+}
