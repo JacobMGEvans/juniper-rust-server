0131a236c81a9a16aa9af0e36b8110230108f0a5
Jacob Evans <cloud887@gmail.com>
2020-07-19 19:00:08 -0500

    I somehow debugged Rust errors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate juniper;
 use std::io;
 use std::sync::Arc;
 
-use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use actix_web::{web, App, Error, error, HttpResponse, HttpServer};
 use futures::future::Future;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
@@ -13,12 +13,6 @@ mod gql_schema;
 use crate::gql_schema::{create_schema, Schema};
 
 fn main() -> io::Result<()> {
-    // HttpServer::new(|| {
-    //     App::new()
-    //         .route("/", web::get().to(index))
-    // })
-    // .bind("localhost:4321")?
-    // .run()
     let schema = std::sync::Arc::new(create_schema());
         HttpServer::new(move || {
             App::new()
@@ -32,16 +26,15 @@ fn main() -> io::Result<()> {
 
 fn graphql(
     st: web::Data<Arc<Schema>>,
-    data: web::Json<GraphQLRequest>,) -> impl Future<Item = HttpResponse , Error = Error>{
+    data: web::Json<GraphQLRequest>,) -> impl Future<Item = HttpResponse , Error = error::Error>{
         web::block(move || {
             let res = data.execute(&st, &());
             Ok::<_,serde_json::error::Error>(serde_json::to_string(&res)?)
-            .map_err(Error::from)
-            .and_then(|user| {
-                Ok(HttpResponse::OK()
-            .content_type("application/json")
-            .body(user))
-            })
+        }).map_err(Error::from)
+        .and_then(|user| {
+            Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(user))
         })
 }
 
